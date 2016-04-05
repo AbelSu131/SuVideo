806186c0c62299b09f3fd6cde21e731ea00219da
AbelSu <abelsu1993@outlook.com>
2016-04-05 08:01:56 +0800

    Initial Commit

---
+++ SuVideo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SuVideo
+//
+//  Created by AbelSu on 16/4/5.
+//  Copyright © 2016年 AbelSu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SuVideo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SuVideo
+//
+//  Created by AbelSu on 16/4/5.
+//  Copyright © 2016年 AbelSu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
